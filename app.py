b53b3b162b7e827cfd015f62e1349148f30209a8
emleonstz <emleons23@gmail.com>
2025-03-19 14:50:29 +0300

    first commit

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request, redirect, url_for
+from models import db, User
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SECRET_KEY'] = 'supersecretkey'
+
+db.init_app(app)
+
+@app.route('/')
+def home():
+    users = User.query.all()
+    return render_template('home.html', users=users)
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/add', methods=['POST'])
+def add_user():
+    name = request.form['name']
+    new_user = User(name=name)
+    db.session.add(new_user)
+    db.session.commit()
+    return redirect(url_for('home'))
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
