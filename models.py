17469ce0d0f6ba7f3bc6f5da22483e629766531c
emleonstz <emleons23@gmail.com>
2025-04-03 11:39:04 +0300

    first commit

--- models.py
+++ models.py
@@ -5,3 +5,4 @@ db = SQLAlchemy()
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
+    country = db.Column(db.String(150), nullable = False)
